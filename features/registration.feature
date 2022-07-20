608e5e2d97b50e9c787b4b9f88eaac6264649183
Kgs. Azzam Nizar <azzam.nizar@kuncie.com>
2022-07-20 18:37:23 +0700

    Adding skeleton BDD

---
+++ features/registration.feature
@@ -0,0 +1,8 @@
+Feature: Registration Feature
+
+    As a visitior I can create a new account by the registration Feature
+
+Scenario: New User Registration E2E Scenario
+Given User already on the registration page
+When User fills out the registration form with correct data
+Then User should see successfull message
---
+++ features/step_definitions/registration_steps.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+const { Given, When, Then } = require('@cucumber/cucumber');
+
+Given('User already on the registration page', function () {
+    // Write code here that turns the phrase above into concrete actions
+  });
+
+  When('User fills out the registration form with correct data', function () {
+    // Write code here that turns the phrase above into concrete actions
+    
+  });
+
+  Then('User should see successfull message', function () {
+    // Write code here that turns the phrase above into concrete actions
+    
+  });
+
